f25a67a45b12f0f7515e7eb7b7884dae9a39e89e
Kalen-ne <54280054+Kalen-ne@users.noreply.github.com>
2020-01-13 13:17:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,9 +12,44 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+@client.event
+async def on_message(message):
+    if client.user in message.mentions
+        reply+f'{message.author.mention}呼びました？'
+        await message.channel.send(reply)
+
 @bot.command(name="riot")
 async def riot(ctx):
     await ctx.send(f"C o m e　i n t o　t h e　w o r l d \n 降　り　立　つ　姿　は \n 絶　大　な　輝　き　の \n f a n t a s t i c　a r t \n 至　高　の　音　楽　を　味　わ　え　と \n L e t　m e　s h o w　y o u \n 酔　い　し　れ　れ　ば　い　い \n 僕　ら　の　音　は \n 世　界　へ　と　憑　依　す   る")
+
+@bot.command(name="opera")
+async def opera(ctx):
+    await ctx.send(f"ｲｸｰｵｰｸｰwwwｲｸｰｾｰﾝｰwwwﾂﾑｲﾀﾞｰwwwｷｾｰｷｰﾆｨｨｨｨｨwww(ｽﾃｪｯﾌﾟwﾊﾞｲwｽﾃｪｯﾌﾟwww)ﾃﾞｪｪｪﾝｾｰﾂｰﾊｰwwwｺｺｶｰﾗｧｧｧwwwwｳﾏｰﾚｰﾀｰwwww")
+
+@bot.command(name="unstoppable")
+async def unstoppable(ctx):
+    await ctx.send(f"ウザったいとジレったいが甘えて\nJust 承認欲求 every day 止まらない\n僕と僕は共犯者さ\n偽物と踊れ踊れ(Lullaby)\n迷いと不安が舌を出して\nコチラを指さし嘲る\nAre you enjoying？Are you excited？\n…Yes？そう見えるの？\nじゃあ…それでいい")
+
+@bot.command(name="atoz")
+async def atoz(ctx):
+    await ctx.send(f"ｴｰﾄｩｰｯｾﾞwｴｰﾄｩｰｯｾﾞwﾃｰwﾝｰwｶｰwｦｰwｴｰﾄｩｰｯｾﾞwｴｰﾄｩｰｯｾﾞwﾂｰwﾅｰwｲｰwﾃﾞｰwｱｯﾊﾟﾚﾅwｼﾞﾝｾｪｲwwｺｯｺﾝﾄｳｻﾞｧｲﾏｧﾃﾞwｱｲｯwwﾕﾒｯwwｻｧｻｧｹﾞﾏｧｯｽwwwｴｰﾄｩｰｯｾﾞwｴｰﾄｩｰｯｾﾞwﾃｰwﾝｰwｶｰwｦｰwｴｰﾄｩｰｯｾﾞwｴｰﾄｩｰｯｾﾞwﾂｰwﾅｰwｲｰwﾃﾞｰwｱｯﾊﾟﾚﾅwｼﾞﾝｾｪｲwwｺｯｺﾝﾄｳｻﾞｧｲﾏｧﾃﾞwｱｲｯwwﾕﾒｯwwｻｧｻｧｹﾞﾏｧｯｽwwwｴｰﾄｩｰｯｾﾞwｴｰﾄｩｰｯｾﾞwﾃｰwﾝｰwｶｰwｦｰwｴｰﾄｩｰｯｾﾞwｴｰﾄｩｰｯｾﾞwﾂｰwﾅｰwｲｰwﾃﾞｰwｱｯﾊﾟﾚﾅwｼﾞﾝｾｪｲwwｺｯｺﾝﾄｳｻﾞｧｲﾏｧﾃﾞwｱｲｯwwﾕﾒｯwwｻｧｻｧｹﾞﾏｧｯｽwwwｴｰﾄｩｰｯｾﾞwｴｰﾄｩｰｯｾﾞwﾃｰwﾝｰwｶｰwｦｰwｴｰﾄｩｰｯｾﾞwｴｰﾄｩｰｯｾﾞwﾂｰwﾅｰwｲｰwﾃﾞｰwｱｯﾊﾟﾚﾅwｼﾞﾝｾｪｲwwｺｯｺﾝﾄｳｻﾞｧｲﾏｧﾃﾞwｱｲｯwwﾕﾒｯwwｻｧｻｧｹﾞﾏｧｯｽwwwｴｰﾄｩｰｯｾﾞwｴｰﾄｩｰｯｾﾞwﾃｰwﾝｰwｶｰwｦｰwｴｰﾄｩｰｯｾﾞwｴｰﾄｩｰｯｾﾞwﾂｰwﾅｰwｲｰwﾃﾞｰwｱｯﾊﾟﾚﾅwｼﾞﾝｾｪｲwwｺｯｺﾝﾄｳｻﾞｧｲﾏｧﾃﾞwｱｲｯwwﾕﾒｯwwｻｧｻｧｹﾞﾏｧｯｽwwwｴｰﾄｩｰｯｾﾞwｴｰﾄｩｰｯｾﾞwﾃｰwﾝｰwｶｰwｦｰwｴｰﾄｩｰｯｾﾞwｴｰﾄｩｰｯｾﾞwﾂｰwﾅｰwｲｰwﾃﾞｰwｱｯﾊﾟﾚﾅwｼﾞﾝｾｪｲwwｺｯｺﾝﾄｳｻﾞｧｲﾏｧﾃﾞwｱｲｯwwﾕﾒｯwwｻｧｻｧｹﾞﾏｧｯｽwwwｴｰﾄｩｰｯｾﾞwｴｰﾄｩｰｯｾﾞwﾃｰwﾝｰwｶｰwｦｰwｴｰﾄｩｰｯｾﾞwｴｰﾄｩｰｯｾﾞwﾂｰwﾅｰwｲｰwﾃﾞｰwｱｯﾊﾟﾚﾅwｼﾞﾝｾｪｲwwｺｯｺﾝﾄｳｻﾞｧｲﾏｧﾃﾞwｱｲｯwwﾕﾒｯwwｻｧｻｧｹﾞﾏｧｯｽwww")
+
+@bot.command(name="kiseki")
+async def kiseki(ctx):
+    await ctx.send(f"ｱ-ﾘｨｨｨwwwｶﾞﾄｫｫｫwww' ﾏﾜﾙwwwﾁｷｭｳwwwｱ-ﾅ-ｧﾀｧﾄｫｫｫ↑ﾜﾀｼｨ↓ﾊｽｽﾑｩｩｩwwwﾆｷﾞﾙｩﾃｪwwwﾊﾅﾚｪｪｪﾃﾓｫwwwｵﾜﾗﾅｨwwwｷｽﾞﾅｶﾞｱﾙｩ↑wwwwwﾀﾞﾝwﾀﾞﾝwﾀﾞﾝwﾀﾞﾝwﾀﾞﾝwﾀﾞﾝwﾄｩwﾃｨﾝ↑wwwww")
+
+@bot.command(name="yakitori")
+async def yakitori(ctx):
+    await ctx.send(f"ﾏﾘﾅｰwwwwﾏﾘﾅｰwwwwwwLALAN(ﾕｳｾﾝ)wwwﾗﾝwﾗﾝwﾗﾝwLALAN(ﾀﾞｲｱﾙｱｯﾌﾟ)wwwwﾗﾝwwﾗﾝwwﾗﾝwwLALAN(ﾑｾﾝ)wwwwwﾗﾝwwwﾗﾝwwwﾗﾝwwwLALAN(ﾐﾀｹ)wwwwwwﾗﾝwwwwﾗﾝwwwwﾗﾝwwww")
+
+@bot.command(name="riot2")
+async def riot2(ctx):
+    await ctx.send(f"┻┳|ﾁﾗｯ\n┳┻|∧\n┻┳|･ω･)\n┳┻|⊂ﾉ\n┻┳|Ｊ\n┳┻|\n┻┳|ｳｽﾞｳｽﾞ\n┳┻|∧\n┻┳|･ω･)\n┳┻|⊂ﾉ\n┻┳|Ｊ\n┳┻|\n┻┳|ｽｯ\n┳┻|∧\n┻┳│･ -・)\n┳┻|⊂ﾉ\n┻┳|Ｊ\n┳┻|\n┻┳|\n┳┻|∧\n┻┳|º дº)< C o m e  t o t h e  w o r l d\n┳┻|⊂ﾉ　      降 り 立 つ 姿 は \n┻┳|Ｊ")
+
+@bot.command(name="arisa")
+async def arisa(ctx):
+    await ctx.send(f"ちょままちょまままっ！ｗｗｗｗｗｗｗｗｗｗｗｗちょまままままま！(戸山香澄を叩く音)ちょままァ！ちょままァァｧｧ！ちょまままァァｧｧ！ｗｗｗｗｗちょままーーー！！(戸山香澄を叩く音)ちょままままままままｗｗｗｗ")
+
     
 
 bot.run(token)
