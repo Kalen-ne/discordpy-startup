a5b961675b684b008a6c2db8de3deb4a97a1e0fc
Kalen-ne <54280054+Kalen-ne@users.noreply.github.com>
2020-01-14 14:19:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,9 +12,9 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@client.event
+@bot.event
 async def on_message(message):
-    if client.user in message.mentions:
+    if bot.user in message.mentions:
         reply = (f"{message.author.mention}呼ばれたのでRIOT歌いますｗｗｗｗC o m e　i n t o　t h e　w o r l d \n 降　り　立　つ　姿　は \n 絶　大　な　輝　き　の \n f a n t a s t i c　a r t \n 至　高　の　音　楽　を　味　わ　え　と \n L e t　m e　s h o w　y o u \n 酔　い　し　れ　れ　ば　い　い \n 僕　ら　の　音　は \n 世　界　へ　と　憑　依　す   る")
         await message.hannel.send(reply)
 
